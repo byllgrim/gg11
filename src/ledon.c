50ca61762ff8b259108317f152d8382561a7f56f
byllgrim <robin.a.t.pedersen@gmail.com>
2017-12-22 00:00:09 +0100

    remove unneeded includes from ledon.c

--- src/ledon.c
+++ src/ledon.c
@@ -1,17 +1,6 @@
 #include "bsp.h"
 #include "em_chip.h"
-
-/*
-TODO from example blink.c
-#include <stdint.h>
-#include <stdbool.h>
 #include "em_device.h"
-#include "em_chip.h"
-#include "em_cmu.h"
-#include "em_emu.h"
-#include "bsp.h"
-#include "bsp_trace.h"
-*/
 
 int
 main(void)
@@ -19,8 +8,8 @@ main(void)
 	CHIP_Init();
 	BSP_LedsInit();
 
-	BSP_LedToggle(1);
 	BSP_LedSet(0);
+	BSP_LedToggle(1);
 
 	for (;;)
 		;
