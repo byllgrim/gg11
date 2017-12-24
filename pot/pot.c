a599a1af895f20c23cb5c04ef42f8a769878f41e
byllgrim <robin.a.t.pedersen@gmail.com>
2017-12-24 03:46:56 +0100

    [pot] add green status light

--- pot/pot.c
+++ pot/pot.c
@@ -1,5 +1,15 @@
+#include <stdbool.h>
+
 #include "em_adc.h"
 #include "em_chip.h"
+#include "em_cmu.h"
+#include "em_gpio.h"
+
+enum {
+	LEDPORT = gpioPortH,
+	REDPIN = 10,
+	GREENPIN = 11
+};
 
 uint32_t
 measurePot(ADC_TypeDef *adc)
@@ -15,6 +25,22 @@ measurePot(ADC_TypeDef *adc)
 	return data;
 }
 
+void
+greenLedOn(void)
+{
+	GPIO_PinOutSet(LEDPORT, REDPIN);
+	GPIO_PinOutClear(LEDPORT, GREENPIN); /* active low */
+}
+
+void
+initLeds(void)
+{
+	CMU_ClockEnable(cmuClock_GPIO, true);
+	GPIO_PinModeSet(LEDPORT, REDPIN, gpioModePushPull, 1);
+	GPIO_PinModeSet(LEDPORT, GREENPIN, gpioModePushPull, 1);
+	greenLedOn();
+}
+
 int
 main(void)
 {
@@ -23,9 +49,10 @@ main(void)
 
 	CHIP_Init();
 	/* TODO enable clocks: HFPERCLK? */
-	ADC_Init(&adc, &adc_init);
+	//ADC_Init(&adc, &adc_init);
+	initLeds();
 
-	measurePot(&adc);
+	//measurePot(&adc);
 
 	for (;;)
 		;
