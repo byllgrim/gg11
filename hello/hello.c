5319a5cbdcb9a9aeb8307a7d8c5bc8bd59070dc8
byllgrim <robin.a.t.pedersen@gmail.com>
2018-01-01 22:46:11 +0100

    complete VCOM example

--- hello/hello.c
+++ hello/hello.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+#include "em_chip.h"
+#include "em_device.h"
+#include "retargetserial.h"
+
 int
 main(void)
 {
-	/* TODO VCOM */
+	CHIP_Init();
+	RETARGET_SerialInit();
+	RETARGET_SerialCrLf(1);
 
-	printf("Hello?\n");
+	printf("Hello, World!\n");
 
 	for (;;)
 		;
